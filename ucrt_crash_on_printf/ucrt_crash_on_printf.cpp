cf997e599c3beac256767f9cc95fac4bcecdcee7
dbjdbj <dbj@dbj.org>
2018-12-01 12:12:22 +0100

    created

--- ucrt_crash_on_printf/ucrt_crash_on_printf.cpp
+++ ucrt_crash_on_printf/ucrt_crash_on_printf.cpp
@@ -1,21 +1,50 @@
-// ucrt_crash_on_printf.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿
+/* 
+created 01.DEC.2018 by dbj@dbj.org
+*/
 #include "pch.h"
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+#include <fcntl.h>
+/*
+----------------------------------------------------------------------------
+	Debug build x86 and x64 crashes with message box:
+	https://snag.gy/z1hXeu.jpg
 
+	Startigng the Release build x86 x64 with F5 from VS 2017 community,
+	crashes in ucrtbase.dll with the following
+	info in VS2017
+	https://snag.gy/nDyxUr.jpg
+
+	Starting with CTRL+F5 does no output whatsoever, it just finishes ...
+----------------------------------------------------------------------------
+SDK 10.0.17763.0
+VS 2017 Community 15.9.3
+WIN10 PRO
+*/
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	// кошка 日本
+	constexpr auto specimen =
+		L"\x043a\x043e\x0448\x043a\x0430 \x65e5\x672c\x56fd";
+
+	int result = _setmode(_fileno(stdout), _O_U8TEXT);
+	if ( result == -1) {
+		perror(__FUNCSIG__ " -- EXIT -- Cannot _setmode(_fileno(stdout), _O_U8TEXT)");
+		exit(EXIT_FAILURE);
+	}
+	// should display: кошка 日本
+	::printf("\nprintf()\n\tspecimen: %S\n", specimen);
+	/*
+	Debug build x86 and x64 crashes with message box:
+	https://snag.gy/z1hXeu.jpg
+
+	Startigng the Release build x86 x64 with F5 from VS 2017 community,
+	crashes in ucrtbase.dll with the following
+	info in VS2017 
+	https://snag.gy/nDyxUr.jpg
+
+	Starting with CTRL+F5 does no output whatsoever, it just finishes ...
+	*/
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
